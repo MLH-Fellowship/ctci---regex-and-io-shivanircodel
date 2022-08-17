e43bf95faf415681882455d6d258e76859b45b3a
Shivani Rajgopal <88949973+shivanircodel@users.noreply.github.com>
2022-08-17 14:52:45 -0400

    Update vowels.py

--- scripts/vowels.py
+++ scripts/vowels.py
@@ -1,1 +1,15 @@
 # Write the solution for "Filter Vowels".
+import re 
+import os 
+from string import * 
+
+print ("opening file")
+
+pattern = "(a|e|i|o|u|A|E|I|O|U)"
+
+with open ('vowels.txt','r') as f: 
+  context = f.read()
+  number_of_vowels = len(re.findall(pattern, context))
+  print(number_of_vowels)
+  string = str(number_of_vowels)
+  os.rename("vowels.txt", "vowels-"+string+".txt")
