9fbe2b2f789df3eb1191359dcc84a29136e2ed1c
Shivani Rajgopal <88949973+shivanircodel@users.noreply.github.com>
2022-08-16 22:19:55 -0400

    Update emails.py

--- scripts/emails.py
+++ scripts/emails.py
@@ -1,1 +1,10 @@
-# Write the solution for "New Emails".
+with open("./inputs/emails.csv", 'r') as file:
+    reader = csv.reader(file.readlines())
+
+with open('emails_solution.csv', 'w') as outf:
+    writer = csv.writer(outf)
+    for line in reader:
+        temp_line = line
+        temp_line[1] = re.sub("@[a-z0-9.-]+(.)?[a-z]{2,4}", "@mlh.io", line[1])
+        writer.writerow(temp_line)
+    writer.writerows(reader)
