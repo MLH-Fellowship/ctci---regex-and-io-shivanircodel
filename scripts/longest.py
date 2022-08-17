7bfc16d44fa052642d37472a766f6cf373815865
Shivani Rajgopal <88949973+shivanircodel@users.noreply.github.com>
2022-08-17 14:47:58 -0400

    Update longest.py

--- scripts/longest.py
+++ scripts/longest.py
@@ -1,1 +1,17 @@
 # Write the solution for "Longest Word".
+import re
+
+f = open("../inputs/longest.txt", "r")
+long_text = f.read()
+f.close()
+
+long_len = 0
+longest = ""
+for word in words:
+    if len(word) > long_len:
+        long_len = len(word)
+        longest = word
+
+  out_file = open("longest_out.txt", "w")
+  out_file.write(long_text)
+  out_file.close()
